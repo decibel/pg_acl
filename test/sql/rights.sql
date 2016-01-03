4784e0782b622d66f2e4d153313c3328da07dc26
Jim Nasby <Jim.Nasby@BlueTreble.com>
2016-01-02 19:32:30 -0600

    Add plan()

--- test/sql/rights.sql
+++ test/sql/rights.sql
@@ -7,6 +7,8 @@ SET search_path=public,tap;
 
 -- NOTE: the compat.sql test verifies that we've got the full set of rights
 
+SELECT plan(6);
+
 -- Sanity-check with grant version
 SELECT is(
   length(_aclitems_all_rights())
